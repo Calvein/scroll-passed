3d79584755705e6291b14f855a38f47a443b29d6
Calvein <calvein@gmail.com>
2014-09-02 11:53:09 +1000

    Add boolean to keep listning and returns id of the event, fix #2

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // Uniq Id underscore like
-var uniqueId = function(prefix) {
-    var id = uniqueId.id++
-    return prefix ? prefix + id : id
-}
+var uniqueId = function() { return uniqueId.id++ }
 uniqueId.id = 1
 
 isScrolledIntoView = function($el) {
@@ -15,13 +12,21 @@ isScrolledIntoView = function($el) {
     return ((elBottom <= docViewBottom) && (elTop >= docViewTop))
 }
 
-module.exports = function($el, cb) {
-    var eventName = 'scroll.' + uniqueId('passed_')
+module.exports = function(options) {
+    var $el = options.$el
+    var cb = options.callback
+    var keepListening = !!options.keepListening
 
+    // Check for mandatory optpions
+    if (!$el || !$el.jquery) throw new Error('A jQuery element must be passed')
+    if (!cb) throw new Error('A callback must be passed')
+
+    var id = uniqueId()
+    var eventName = 'scroll.passed_' + id
     $(window).on(eventName, function(e) {
         if (isScrolledIntoView($el)) {
-            $(window).off(eventName)
-            cb($el)
+            if (!keepListening) $(window).off(eventName)
+            cb($el, id)
         }
     })
 }
